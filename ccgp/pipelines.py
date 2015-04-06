a6aec84e9ef78a6c407693de076982a23ab1eaff
David Wong <stef-hw@163.com>
2015-04-06 12:50:52 +0800

    up

--- ccgp/pipelines.py
+++ ccgp/pipelines.py
@@ -20,7 +20,7 @@ class CcgpPipeline(object):
 
     def process_item(self, item, spider):
         if not self.redis.sismember('base', item['url']):
-            base_id = self.db.insert("insert into base(url, title, zone, content, publish_time, source) values(%s, %s, %s, %s, %s)", 
+            base_id = self.db.insert("insert into base(url, title, zone, content, publish_time, source) values(%s, %s, %s, %s, %s, %s)", 
                                      item['url'], item['title'].encode('utf-8'), item['zone'].encode('utf-8'), item['content'].encode('utf-8'), item['publish_time'], item['source'])
             if base_id:
                 for atts in item['attachments']:
