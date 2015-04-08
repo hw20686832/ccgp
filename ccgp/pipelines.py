535a76e5fefc021f77d120de39b8efa38b623a8d
hw20686832 <stef-hw@163.com>
2015-04-08 11:10:28 +0800

    up

--- ccgp/pipelines.py
+++ ccgp/pipelines.py
@@ -6,6 +6,7 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import re
 import urllib
+import traceback
 from StringIO import StringIO
 from urlparse import urljoin
 
@@ -27,40 +28,43 @@ class CcgpPipeline(object):
         return cls(settings)
 
     def process_item(self, item, spider):
+        self.db.execute("start transaction")
         if not self.redis.sismember('base', item['url']):
             base_id = self.db.insert("insert into base(url, title, zone, content, publish_time, source) values(%s, %s, %s, %s, %s, %s)",
                                      item['url'], item['title'].encode('utf-8'), item['zone'].encode('utf-8'), item['content'].encode('utf-8'), item['publish_time'], item['source'])
             if base_id:
-                for atts in item['attachments']:
-                    if 'documentView.do?method=downFile' in atts['url']:
-                        session = requests.Session()
-                        response = session.get(atts['url'])
-                        q = _Q(response.content)
-                        params = {}
-                        for i in q("form input"):
-                            if i.name:
-                                params[i.name] = i.value
+                try:
+                    for atts in item['attachments']:
+                        if 'documentView.do?method=downFile' in atts['url']:
+                            session = requests.Session()
+                            response = session.get(atts['url'])
+                            q = _Q(response.content)
+                            params = {}
+                            for i in q("form input"):
+                                if i.name:
+                                    params[i.name] = i.value
 
-                        img_url = q('form img').attr('src')
-                        response = session.get(urljoin("http://www.cqgp.gov.cn/", img_url))
-                        imgio = StringIO(response.content)
-                        code = vcode.analyze(imgio)
-                        params['imageString'] = code
+                            img_url = q('form img').attr('src')
+                            response = session.get(urljoin("http://www.cqgp.gov.cn/", img_url))
+                            imgio = StringIO(response.content)
+                            code = vcode.analyze(imgio)
+                            params['imageString'] = code
 
-                        post_url = q('form').attr('action')
-                        response = session.post(urljoin("http://www.cqgp.gov.cn/", post_url), data=params)
-                        filename = re.search('attachment; filename="(.*?)"',
-                                             response.headers['content-disposition']).group(1)
-                        filename = urllib.unquote(filename)
-                        self.db.insert("insert into attachments(url, base_id, name, file) values(%s, %s, %s, %s)",
-                                       atts['url'].encode('utf-8'), base_id, filename, torndb.MySQLdb.Binary(response.content))
-                    else:
-                        try:
+                            post_url = q('form').attr('action')
+                            response = session.post(urljoin("http://www.cqgp.gov.cn/", post_url), data=params)
+                            filename = re.search('attachment; filename="(.*?)"',
+                                                 response.headers['content-disposition']).group(1)
+                            filename = urllib.unquote(filename)
+                            self.db.insert("insert into attachments(url, base_id, name, file) values(%s, %s, %s, %s)",
+                                           atts['url'].encode('utf-8'), base_id, filename, torndb.MySQLdb.Binary(response.content))
+                        else:
                             response = requests.get(atts['url'])
                             self.db.insert("insert into attachments(url, base_id, name, file) values(%s, %s, %s, %s)",
                                            atts['url'].encode('utf-8'), base_id, atts['name'].encode('utf-8'), torndb.MySQLdb.Binary(response.content))
-                        except Exception as e:
-                            continue
-
-            self.redis.sadd('base', item['url'])
-            return base_id
+                except Exception as e:
+                    traceback.print_exc()
+                    self.db.execute("rollback")
+                else:
+                    self.db.execute("commit")
+                    self.redis.sadd('base', item['url'])
+            return item['title']
